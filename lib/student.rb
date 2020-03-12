1ca21d57ea3b6e400e1d7559a1821235b9bbd25a
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-12 13:30:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -113,6 +113,14 @@ class Student
   end
   
   def self.all_students_in_grade_X(grade)
-    
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+    SQL
+ 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 end
