d6545ff6eb273565a102ef044ece62d26e64743e
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-12 13:17:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -88,7 +88,15 @@ class Student
   end
   
   def self.first_X_students_in_grade_10(num)
-    
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+    SQL
+ 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
   end
   
   def self.first_student_in_grade_10 
