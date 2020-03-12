e6474d3e47150910e44ca9b6ef09e2c659bd278e
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-12 13:06:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -76,7 +76,13 @@ class Student
   end
   
   def self.students_below_12th_grade
-    
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12
+    SQL
+ 
+    DB[:conn].execute(sql)
   end
   
   def self.first_X_students_in_grade_10(num)
