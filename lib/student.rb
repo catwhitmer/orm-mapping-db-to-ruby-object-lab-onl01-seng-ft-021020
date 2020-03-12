a5e292b9ce3111570a0ea57c4d2aa4ba85ccc545
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-12 13:12:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -84,8 +84,7 @@ class Student
  
     B[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
-    end.first #first element of the array
-  end
+    end
   end
   
   def self.first_X_students_in_grade_10(num)
