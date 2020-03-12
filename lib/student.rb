e5a489fe64b4cf6e0f4d361b3c80f2e07a4651a7
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-12 13:15:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -82,9 +82,7 @@ class Student
       WHERE grade < 12
     SQL
  
-    B[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
+    B[:conn].execute(sql)
   end
   
   def self.first_X_students_in_grade_10(num)
