b09573374b2eb14f554521b053a9c7e76631a883
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-12 13:25:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -91,7 +91,8 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = 10 LIMIT ?
+      WHERE grade = 10 
+      LIMIT 1
     SQL
  
     DB[:conn].execute(sql, num)
